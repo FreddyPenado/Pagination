427b9e07b70d8a146a4cbbad9c53cd66f1581478
FreddyPenado <penadofreddy@gmail.com>
2020-06-24 10:58:11 -0600

    HTML and CSS Structure

---
+++ index.view.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagination</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h1>Articles</h1>
+    <section class="articles">
+        <ul>
+            <li>1.-Lorem ipsum dolor sit amet, consectetur adipiscing elit!</li>
+            <li>2.-Lorem ipsum dolor sit amet, consectetur adipiscing elit!</li>
+            <li>3.-Lorem ipsum dolor sit amet, consectetur adipiscing elit!</li>
+            <li>4.-Lorem ipsum dolor sit amet, consectetur adipiscing elit!</li>
+        </ul>
+    </section>
+
+    <section class="pagination">
+        <ul>
+            <li class="disable">&laquo;</a></li>
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li>&raquo;</a></li>
+        </ul>
+    </section>
+</div>
+</body>
+</html>

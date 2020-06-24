62d77b1bcfe9defce6de1b9d15c792280f030258
FreddyPenado <penadofreddy@gmail.com>
2020-06-24 12:47:54 -0600

    pagination logic

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-require "index.view.php";
+
+try {
+    $conection = new PDO('mysql:host=localhost; dbname=paginacion', 'root','');
+
+}catch (PDOException $e){
+    echo "ERROR:" . $e->getMessage();
+    die();
+}
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$postPerPage = 5;
+
+$start = ($page > 1) ? ($page * $postPerPage - $postPerPage) : 0;
+
+$articles = $conection->prepare("SELECT sql_calc_found_rows  * FROM articulos LIMIT $start, $postPerPage");
+$articles->execute();
+$articles = $articles->fetchAll();
+//print_r($articles);
+if (!$articles){
+    header('Location: index.php');
+}
+$totalarticles = $conection-> query('SELECT FOUND_ROWS() as total ');
+$totalarticles = $totalarticles->fetch()['total'];
+
+$pagenum = ceil($totalarticles / $postPerPage) ;
+echo $pagenum;
+ require "index.view.php";
+
